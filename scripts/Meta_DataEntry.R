f8568df47d23da16b48b492601530c0d2a1da524
Nicole E Soltis <ne.soltis@gmail.com>
2016-04-06 11:41:01 -0700

    starting meta-analysis with Sl, Br, Ce, Ci, Gm, Hc

---
+++ scripts/Meta_DataEntry.R
@@ -0,0 +1,31 @@
+#Meta-analysis of all Bc x eudicot experiments 
+#040516
+#-------------------------------------------------------------
+rm(list=ls())
+setwd("~/Projects/BcEudicotGWAS/data/MetaAnalysis")
+#-------------------------------------------------------------
+#first get all data frames to match up
+SlModDat <- read.csv("SlModelData.csv")
+BrModDat <- read.csv("BrModelData.csv")
+CeModDat <- read.csv("CeModelData.csv")
+CiModDat <- read.csv("CiModelData.csv")
+GmModDat <- read.csv("GmModelData.csv")
+HcModDat <- read.csv("HcModelData.csv")
+
+#data to include: Exp, Rep, Flat, Domest, PlantGeno, IndPlant, IsolateID, Scale.LS
+library(plyr)
+#format: new = old
+SlModDat <- dplyr::select(SlModDat, Exp = ExpBlock, Rep = PExpRep.x, Flat = AgFlat, Domest = Species, PlantGeno = PlGenoNm, IndPlant = IndPlant, IsolateID = Igeno, Scale.LS = Scale.LS)
+BrModDat <- dplyr::select(BrModDat, Exp = Exp, Rep = Rep, Flat = ImageName, Domest = Domestication, PlantGeno = PlantID, IndPlant = PnumID, IsolateID = IsolateID, Scale.LS = Scale.LS)
+CeModDat <- dplyr::select(CeModDat, Exp = Exp, Rep = Rep, Flat = ImageName, Domest = Domestication, PlantGeno = PlantID, IndPlant = PnumID, IsolateID = IsolateID, Scale.LS = Scale.LS)
+CiModDat <- dplyr::select(CiModDat, Exp = Exp, Rep = Rep, Flat = ImageName, Domest = Domestication, PlantGeno = PlantID, IndPlant = PnumID, IsolateID = IsolateID, Scale.LS = Scale.LS)
+GmModDat <- dplyr::select(GmModDat, Exp = exp, Rep = rep, Flat = Image, Domest = origin, PlantGeno = genotypename, IndPlant = plantnumber, IsolateID = isolatename, Scale.LS = Scale.LS)
+HcModDat <- dplyr::select(HcModDat, Exp = Exp, Rep = Rep, Flat = ImageName, Domest = Domestication, PlantGeno = PlantID, IndPlant = PnumID, IsolateID = IsolateID, Scale.LS = Scale.LS)
+
+write.csv(SlModDat, "SlMetaDat.csv")
+write.csv(BrModDat, "BrMetaDat.csv")
+write.csv(CeModDat, "CeMetaDat.csv")
+write.csv(CiModDat, "CiMetaDat.csv")
+write.csv(GmModDat, "GmMetaDat.csv")
+write.csv(HcModDat, "HcMetaDat.csv")
+
---
+++ scripts/Meta_Plots.R
@@ -0,0 +1,8 @@
+#Meta-analysis of all Bc x eudicot experiments 
+#040516
+#-------------------------------------------------------------
+rm(list=ls())
+setwd("~/Projects/BcEudicotGWAS/data/MetaAnalysis")
+#-------------------------------------------------------------
+MyData <- read.csv("FULLMetaDat.csv")
+
