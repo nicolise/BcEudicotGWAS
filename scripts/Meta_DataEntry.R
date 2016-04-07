e1c61bb09dbebddf649e0d65d6ab56d08216604a
Nicole E Soltis <ne.soltis@gmail.com>
2016-04-06 17:38:10 -0700

    early meta analysis plots: vioplot, scatter

--- scripts/Meta_DataEntry.R
+++ scripts/Meta_DataEntry.R
@@ -16,11 +16,18 @@ HcModDat <- read.csv("HcModelData.csv")
 library(plyr)
 #format: new = old
 SlModDat <- dplyr::select(SlModDat, Exp = ExpBlock, Rep = PExpRep.x, Flat = AgFlat, Domest = Species, PlantGeno = PlGenoNm, IndPlant = IndPlant, IsolateID = Igeno, Scale.LS = Scale.LS)
+SlModDat$Taxon <- "Solanum"
 BrModDat <- dplyr::select(BrModDat, Exp = Exp, Rep = Rep, Flat = ImageName, Domest = Domestication, PlantGeno = PlantID, IndPlant = PnumID, IsolateID = IsolateID, Scale.LS = Scale.LS)
+BrModDat$Taxon <- "Brapa"
 CeModDat <- dplyr::select(CeModDat, Exp = Exp, Rep = Rep, Flat = ImageName, Domest = Domestication, PlantGeno = PlantID, IndPlant = PnumID, IsolateID = IsolateID, Scale.LS = Scale.LS)
+CeModDat$Taxon <- "Cendivia"
 CiModDat <- dplyr::select(CiModDat, Exp = Exp, Rep = Rep, Flat = ImageName, Domest = Domestication, PlantGeno = PlantID, IndPlant = PnumID, IsolateID = IsolateID, Scale.LS = Scale.LS)
+CiModDat$Taxon <- "Cintybus"
 GmModDat <- dplyr::select(GmModDat, Exp = exp, Rep = rep, Flat = Image, Domest = origin, PlantGeno = genotypename, IndPlant = plantnumber, IsolateID = isolatename, Scale.LS = Scale.LS)
+GmModDat$Taxon <- "Glycine"
+GmModDat$IndPlant <- as.factor(GmModDat$IndPlant)
 HcModDat <- dplyr::select(HcModDat, Exp = Exp, Rep = Rep, Flat = ImageName, Domest = Domestication, PlantGeno = PlantID, IndPlant = PnumID, IsolateID = IsolateID, Scale.LS = Scale.LS)
+HcModDat$Taxon <- "Helianthus"
 
 write.csv(SlModDat, "SlMetaDat.csv")
 write.csv(BrModDat, "BrMetaDat.csv")
@@ -29,3 +36,5 @@ write.csv(CiModDat, "CiMetaDat.csv")
 write.csv(GmModDat, "GmMetaDat.csv")
 write.csv(HcModDat, "HcMetaDat.csv")
 
+FullMetaDat <- rbind(SlModDat, BrModDat, CeModDat, CiModDat, HcModDat, GmModDat)
+write.csv(FullMetaDat, "FULLMetaDat.csv")
--- scripts/Meta_Plots.R
+++ scripts/Meta_Plots.R
@@ -4,5 +4,66 @@
 rm(list=ls())
 setwd("~/Projects/BcEudicotGWAS/data/MetaAnalysis")
 #-------------------------------------------------------------
+#load data
 MyData <- read.csv("FULLMetaDat.csv")
+#rename so that Domest is Domesticated vs. Wild vs. landraces
+MyData$Domest[MyData$Domest == 'D'] <- 'Domesticated'
+MyData$Domest[MyData$Domest == 'Dm'] <- 'Domesticated'
+MyData$Domest[MyData$Domest == 'W'] <- 'Wild'
+MyData$Domest[MyData$Domest == 'Wl'] <- 'Wild'
+
+#-------------------------------------------------------------
+#violin plot of lesion size by plant species
+attach(MyData)
+library(ggplot2)
+p <- ggplot(MyData, aes(factor(Taxon), Scale.LS, fill=Domest))
+p + geom_violin(trim=T)+
+  ylim(0,5) +
+  theme( axis.title.x = element_blank()) +
+  ylab("Scaled Lesion Size")+
+ geom_boxplot(width=0.1, position = position_dodge(width = 0.9))
+
+#-------------------------------------------------------------
+##scatterplot by plant species
+names(MyData)
+attach(MyData)
+
+MyData$avgLS <- ave(MyData$Scale.LS, MyData$IsolateID)
+#add PlantNum as an integer sorted by mean lesion size
+library(plyr)
+FigDat <- ddply(MyData, c("PlantGeno", "IsolateID", "Domest", "Taxon", "avgLS"), summarise,
+                 mLS   = mean(Scale.LS))
+#"mean" is mean lesion size by plant genotype.
+MDmeans <- ddply(MyData, c("PlantGeno","Domest", "Taxon"), summarise, mean=mean(Scale.LS))
+#add TxMean to scale lesion size by taxon
+MDmeans$TxMean <- ave(MDmeans$mean, MDmeans$Taxon)
+MDmeans <- MDmeans[order(MDmeans$Taxon, MDmeans$Domest, MDmeans$mean),] 
+MDmeans$PlNum <- c(1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6)
+FigDat <- merge(FigDat, MDmeans[,c(1,4:6)], by="PlantGeno")
+FigDat <- dplyr::select(FigDat, PlOrder = mean, matches("."))
+FigDat$PlOrder <- as.numeric(FigDat$PlOrder)
+library(ggplot2)
+attach(FigDat)
+names(FigDat)
+
+#scale mLs by Taxon
+FigDat$mLS <- (FigDat$mLS / FigDat$TxMean)
+
+#add a column of mmLS (mean of mean lesion size) per isolate
+#sort dataframe by mmLS 
+#then color by the new factor mmLS
+FigDat$mmLS <- ave(FigDat$mLS, FigDat$IsolateID)
+attach(FigDat)
+FigDat <- FigDat[order(mmLS),]
+ggplot(FigDat, aes(x = PlNum, y = mLS))+
+  geom_point()+
+  theme_bw()+
+  geom_line(size=1, aes(color=factor(mmLS), group=factor(IsolateID)), show.legend=F)+
+  #scale_x_discrete(breaks=c("1","2","3", "4", "5", "6", "1", "2", "3", "4", "5", "6"),
+                   #labels=c("LA4345", "LA3008", "LA4355", "LA2706", "LA3475", "LA0410", "LA1547", "LA2093", "LA1684", "LA1589", "LA0480", "LA2176"))+
+  facet_grid(~Taxon*Domest, scales="fixed", space="free_x")+
+  theme(text = element_text(size=24), axis.text.x = element_text(angle = 45, hjust = 1))+
+  labs(y=expression(Lesion ~ Area ~ (cm^{2})), x=element_blank())
+#+geom_smooth(aes(group = 2), size = 2, method = "lm", se = T)
+#----------------------------------------------------------------
 
